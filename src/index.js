4b28e958efbbec613c62e1a0b9b098daf4c36aad
rouvenWikom <r.diener@wikom.de>
2017-04-25 10:26:47 +0200

    allowing multiple children, allowing text nodes as children, using own props instead of child props for condition, introducing passProps and wrapperTag props, allow configuring default values globally via configure() function

--- src/index.js
+++ src/index.js
@@ -6,8 +6,42 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 
-const ConditionalComponent = ({condition, children, state}) =>
-    condition(children.props, state) === true ? children : null;
+const DEFAULTS = {
+    passProps: false,
+    wrapperTag: 'span'
+};
+
+const configure = ({passProps = DEFAULTS.passProps, wrapperTag = DEFAULTS.wrapperTag}) => {
+    DEFAULTS.passProps = passProps;
+    DEFAULTS.wrapperTag = wrapperTag;
+};
+
+const renderChild = (child, props, {passProps, wrapperTag}) => {
+    if (typeof child === 'string')
+        return React.createElement(wrapperTag, null, child);
+
+    if (React.isValidElement(child) === false)
+        return null;
+
+    if (passProps === true)
+        return React.cloneElement(child, props);
+
+    return child;
+};
+
+const ConditionalComponent = ({condition, children, state, dispatch, passProps = DEFAULTS.passProps, wrapperTag = DEFAULTS.wrapperTag, ...props}) => {
+    if (condition(props, state) !== true)
+        return null;
+
+    if (children instanceof Array)
+        return React.createElement(
+            wrapperTag,
+            null,
+            React.Children.map(children, child => renderChild(child, props, {passProps, wrapperTag}))
+        );
+
+    return renderChild(children, props, {passProps, wrapperTag});
+};
 
 const mapStateToProps = state => ({
     state: state
@@ -17,7 +51,10 @@ const Conditional = connect(mapStateToProps)(ConditionalComponent);
 
 Conditional.propTypes = {
     condition: PropTypes.func.isRequired,
-    children: PropTypes.element.isRequired
+    passProps: PropTypes.bool,
+    wrapperTag: PropTypes.string,
+    children: PropTypes.node.isRequired
 };
 
 export default Conditional;
+export {Conditional, configure};
